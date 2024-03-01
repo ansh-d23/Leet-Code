691ecad521b78f306f47ba3936aa6c5674ad1f0c
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-03-01 12:13:53 +0530

    Time: 7 ms (31.42%), Space: 8.2 MB (68.48%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        bool firstone=false;
+        /*bool firstone=false;
         int n=s.length()-1;
         int k=0;
         int p=0;
@@ -16,6 +16,18 @@ public:
                 k++; p++;
                 i++;
             }
+        }
+        return s;*/
+        int count=0;
+        int n=s.length();
+        for(auto &it : s) { 
+            if(it == '1') count++;
+            it = '0'; 
+        }
+        s[n-1]='1';
+        count--;
+        for(int i=0;i<count;i++){
+            s[i]='1';
         }
         return s;
     }
