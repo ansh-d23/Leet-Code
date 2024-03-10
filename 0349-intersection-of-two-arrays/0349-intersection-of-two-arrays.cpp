769419453a761cdf56e5d6df6e0aa75715c725fc
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-03-10 13:16:29 +0530

    Time: 9 ms (25.3%), Space: 13.5 MB (12.68%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
+        /*sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
         vector<int> ans;
         int i = 0, j = 0;
@@ -24,6 +24,17 @@ public:
                 j++;
             }
         }
-        return ans;
+        return ans;*/
+        unordered_set<int> s1;
+        vector<int> ans;
+        unordered_set<int> s2;
+        for(int i=0;i<nums1.size();i++) s1.insert(nums1[i]);
+        for(int i=0;i<nums2.size();i++){
+            if(s1.find(nums2[i])!=s1.end()){
+                s2.insert(nums2[i]);
+            }
+        }
+       for(auto it : s2) ans.push_back(it);
+       return ans;
     }
 };
