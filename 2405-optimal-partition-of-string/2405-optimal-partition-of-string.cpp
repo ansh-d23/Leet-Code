ec10b62312d2a8bf300131ce14fe6f036b05b621
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-03-18 13:44:57 +0530

    Time: 158 ms (43.19%), Space: 47 MB (30.04%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -3,15 +3,12 @@ public:
     int partitionString(string s) {
         unordered_set<char> mpp;
         int count=0;
-        string temp;
         for(int i=0;i<s.length();i++){
             if(mpp.find(s[i])!=mpp.end()){
                 count++;
-                temp="";
                 mpp.clear();
             }
             mpp.insert(s[i]);
-            temp+=s[i];
         }
         return count+1;
     }
