f936ccd87d66d5c6dc1a2051589f3bc0a7ff5e78
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-03-23 12:14:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string res;
+        for(int i=0;i<s.length();i++){
+            int temp=s[i];
+            if((temp>='a' && temp<='z') || (temp>='A' && temp<='Z')){
+                if(temp>='A' && temp<='Z'){
+                    temp+=32;
+                }
+                char r = temp;
+                res+=r;
+            }
+        }
+        cout<<res;
+
+        int left=0;
+        int right=res.length()-1;
+        while(left<=right){
+            if(res[left]!=res[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
