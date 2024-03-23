15f82d6fc4abbf4635bb0377aa5961d3ec2327fc
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-03-23 12:16:34 +0530

    Time: 0 ms (100%), Space: 9.2 MB (22.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         string res;
         for(int i=0;i<s.length();i++){
             int temp=s[i];
-            if((temp>='a' && temp<='z') || (temp>='A' && temp<='Z')){
+            if((temp>='a' && temp<='z') || (temp>='A' && temp<='Z')  || (temp>='0' && temp<='9')){
                 if(temp>='A' && temp<='Z'){
                     temp+=32;
                 }
