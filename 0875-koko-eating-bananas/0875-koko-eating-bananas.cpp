779425cd51121cb3f2c63f8121ee9c767432c256
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-05-06 17:17:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int findmax(vector<int>& piles){
-        int maxi=INT_MIN;
+    int calc_h(vector<int> piles,double mid){
+        int ans=0;
         for(int i=0;i<piles.size();i++){
-            maxi = max(maxi,piles[i]);
-        }
-        return maxi;
-    }
-
-    double caclhr(vector<int>& piles, double h){
-        double ans=0;
-        for(int i=0;i<piles.size();i++){
-            ans+= ceil((double)piles[i]/(double)h);
+            double temp= (piles[i]/mid);
+            temp=ceil(temp);
+            ans+=temp;
         }
         return ans;
     }
 
-    int minEatingSpeed(vector<int>& piles, double h) {
-        int low=0; int high = findmax(piles);
-        while(low<=high){
-            int mid = (high+low)/2;
-            double totalh = caclhr(piles,mid);
-            if(totalh<=h){
-                high=mid-1;
-            }else{
-                low=mid+1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+         sort(piles.begin(),piles.end());
+         int start=1;
+         int end=piles[piles.size()-1];
+         while(start<=end){
+            int mid= start+ (end-start)/2;
+            cout<<start<<" "<<end <<endl;
+            int expected_h = calc_h(piles,mid);
+            if(expected_h==h){
+                return mid;
             }
-        }
-        return low;
+            else if(expected_h>h) start=mid+1;
+            else end=mid-1;
+         }
+         return -1;
     }
 };
