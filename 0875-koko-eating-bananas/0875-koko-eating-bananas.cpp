c8ce7c0222d13213d63b20006d26132a49c260f7
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-05-13 00:04:47 +0530

    Time: 46 ms (5.93%), Space: 38.2 MB (6.08%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,9 +3,7 @@ public:
     int calc_h(vector<int> piles,double mid){
         int ans=0;
         for(int i=0;i<piles.size();i++){
-            double temp= (piles[i]/mid);
-            temp=ceil(temp);
-            ans+=temp;
+            ans+=(piles[i]+mid-1)/mid; // ceil value
         }
         return ans;
     }
@@ -14,16 +12,12 @@ public:
          sort(piles.begin(),piles.end());
          int start=1;
          int end=piles[piles.size()-1];
-         while(start<=end){
+         while(start<end){
             int mid= start+ (end-start)/2;
-            cout<<start<<" "<<end <<endl;
             int expected_h = calc_h(piles,mid);
-            if(expected_h==h){
-                return mid;
-            }
-            else if(expected_h>h) start=mid+1;
-            else end=mid-1;
+            if(expected_h>h) start=mid+1;
+            else end=mid;
          }
-         return -1;
+         return start;
     }
 };
