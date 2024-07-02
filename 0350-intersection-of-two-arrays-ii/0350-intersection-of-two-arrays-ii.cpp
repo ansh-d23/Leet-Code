04e2d989b12a70c3e82397851ef5f59c9d9e44a3
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-07-02 21:54:04 +0530

    Time: 9 ms (11.77%), Space: 14.1 MB (21.98%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-
-        unordered_map<int,int>  mpp1;
-        unordered_map<int,int> mpp2;
+        unordered_map<int, int> mpp;
         vector<int> ans;
 
-        for(int i=0;i<nums1.size();i++) mpp1[nums1[i]]++;
-        for(int i=0;i<nums2.size();i++) mpp2[nums2[i]]++;
+        for (int num : nums1) {
+            mpp[num]++;
+        }
 
-            for(auto it2 : mpp2){
-                if(mpp1.find(it2.first) != mpp1.end()){
-                    auto it1  = mpp1.find(it2.first);
-                    int temp = min(it1->second,it2.second);
-
-                    for(int i=0;i<temp;i++) ans.push_back(it2.first);
-                }
+        for (int num : nums2) {
+            if (mpp[num] > 0) {
+                ans.push_back(num);
+                mpp[num]--;
             }
-            
+        }
 
         return ans;
-        
     }
 };
