c086c3e505dfd92860369d565073725e3a3bacc4
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-07-26 13:57:54 +0530

    Time: 2283 ms (5.01%), Space: 419.5 MB (14.14%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int memo(int i,int j,string s, string t,vector<vector<int>>& dp){
+        
+        if(j<0) return 1;
+        if(i<0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i]==t[j]){
+            int cond1 = memo(i-1,j-1,s,t,dp);
+            int cond2 = memo(i-1,j,s,t,dp);
+            return dp[i][j] = cond1+cond2;
+        }else{
+            return dp[i][j] = memo(i-1,j,s,t,dp);
+        }
+    }
+    int numDistinct(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+
+        return memo(n-1,m-1,s,t,dp);
+    }
+};
