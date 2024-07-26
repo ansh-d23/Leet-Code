2ebbf49e4f18a3f0187f48dc4c8f8bf668871d4b
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-07-26 14:08:08 +0530

    Time: 19 ms (57.86%), Space: 31.7 MB (23.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int tab(string s,string t,vector<vector<unsigned long long>>& dp){
+        int n = s.length();
+        int m = t.length();
+
+        for(int i =0;i<=n;i++) dp[i][0] = 1 ;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
     int memo(int i,int j,string s, string t,vector<vector<int>>& dp){
         
         if(j<0) return 1;
@@ -19,8 +37,8 @@ public:
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
 
-        return memo(n-1,m-1,s,t,dp);
+        return tab(s,t,dp);
     }
 };
