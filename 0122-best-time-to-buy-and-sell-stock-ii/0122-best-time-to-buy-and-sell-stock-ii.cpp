8df8b2f204fbedd339cdd085b63408e743fcd9bf
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2024-07-28 13:02:51 +0530

    Time: 7 ms (44.57%), Space: 20.6 MB (7.88%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,7 +3,7 @@ public:
     int memo(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
 
         if(i==prices.size()) return 0;
-
+        if(dp[i][buy] != -1) return dp[i][buy];
 
         int profit = 0;
         if(buy){
@@ -12,13 +12,13 @@ public:
             profit = max(prices[i] + memo(i+1,1,prices,dp), memo(i+1,0,prices,dp));
         }
 
-        return profit;
+        return dp[i][buy] = profit;
     }
 
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(2,-1));
         return memo(0,1,prices,dp);
 
     }
