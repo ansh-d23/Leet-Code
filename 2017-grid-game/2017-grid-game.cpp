be4e6842da830d1439cd93632d03256731d1686a
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-01-21 11:15:01 +0530

    Time: 0 ms (100%), Space: 66.8 MB (61.54%) - LeetHub

--- 2017-grid-game/2017-grid-game.cpp
+++ 2017-grid-game/2017-grid-game.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        int n = grid[0].size();
-        vector<long long> pre(n,grid[0][0]);
-        vector<long long> post(n,grid[1][n-1]);
+        long long min_result = LLONG_MAX;  
+        long long row1_sum = accumulate(grid[0].begin(), grid[0].end(), 0LL); 
+        long long row2_sum = 0;
 
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]+grid[0][i];
+        for (int i = 0; i < grid[0].size(); ++i) {
+            row1_sum -= grid[0][i];
+            min_result = min(min_result, max(row1_sum, row2_sum));  
+            row2_sum += grid[1][i]; 
         }
-        for(int i=n-2;i>=0;i--){
-            post[i] = post[i+1]+grid[1][i];
-        }
-        long long res= LONG_MAX;
-        for(int i=0;i<n;i++){
-            /*int temp=post[i]+pre[i];
-            if(temp>res){
-                res=temp;
-                ans = max(post[0]-post[i],pre[n-1]-pre[i]);
-            }*/
-            res = min(res,max(post[0]-post[i],pre[n-1] - pre[i]));
-        }
-        return res;
+
+        return min_result; 
     }
 };
