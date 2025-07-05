f8c639064784ca78e2e9e29d66950d2d16a7798d
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-07-05 15:36:57 +0530

    Time: 175 ms (6.75%), Space: 158.1 MB (5.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,27 +3,17 @@ public:
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
-        int maxm = prices[n-1];
+        vector<vector<int>> dp(n,vector<int>(2,0));
 
-        vector<int> max_right(n);
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
 
-        max_right[n-1] = maxm;
-
-        for(int i=n-1;i>0;i--){
-            if(prices[i]>=maxm){
-                maxm = prices[i];
-            }
-            max_right[i-1] = maxm;
-        }
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            if(max_right[i] - prices[i] > ans){
-                ans = max_right[i] - prices[i];
-            }
+        for(int i=1;i<n;i++){
+            dp[i][0] = max(dp[i-1][0], -prices[i]);
+            dp[i][1] = max(prices[i]+dp[i-1][0], dp[i-1][1]);
         }
 
-        return ans;
+        return dp[n-1][1];
         
     }
 };
