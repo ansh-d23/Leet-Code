4fa74f3ea5121a53fbbd9b19794e53ba72681737
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-07-18 13:08:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         vector<vector<int>> temp;
-         int n = intervals.size();
-         sort(intervals.begin(),intervals.end());
-         int start=intervals[0][0],end=intervals[0][1];
-    vector<int> ans;
-        
-         for(int i=0;i<n;i++){
-             
-             int x=intervals[i][0];
-             int y=intervals[i][1];
-             if(x<start && y<start){
-                 //exit
-                ans.push_back(x);
-                ans.push_back(y);
-                temp.push_back(ans);
-                ans.clear();
-             }else
-              if(x>end){
-                ans.push_back(start);
-                ans.push_back(end);
-                temp.push_back(ans);
-                ans.clear();
-                start=x; end=y; 
-              }else{
-                  if(x<start){
-                      start=x;
-                  }
-                  if(y>end){
-                  end=y;
-                  }
-              }
 
-              if(i==n-1){
-                  //exit
-                ans.push_back(start);
-                ans.push_back(end);
-                temp.push_back(ans);
-              }
-         }
-         return temp;
+        int n = intervals.size();
+        vector<vector<int>> ans;
+
+        sort(intervals.begin(),intervals.end());
+
+        for(int i=1;i<n;i++){
+            if(intervals[i-1][1] >= intervals[i][0]){
+                intervals[i][0] = intervals[i-1][0];
+            }else{
+                ans.push_back(intervals[i-1]);
+            }
+        }
+
+        ans.push_back(intervals[n-1]);
+
+        return ans;
     }
 };
