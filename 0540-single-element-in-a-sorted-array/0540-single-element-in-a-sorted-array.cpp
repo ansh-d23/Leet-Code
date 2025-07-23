1427d1280196457bc72d23f31c974f2d1e5b5148
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-07-23 15:48:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,26 +6,25 @@ public:
 
         if(n==1) return nums[0];
         if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+        if(nums[n-1]!=nums[n-2]) return nums[n-2];
 
-        int low=1;
-        int high = n-2;
+        int low=0;
+        int high=n-2;
 
         while(low<=high){
-
-            int mid = (high+low)/2;
+            int mid = low + (high-low)/2;
 
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
 
-            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])){
-                low=mid+1;
-            }else{
+            if((mid%2==0 && nums[mid]==nums[mid-1]) || (mid%2==1 && nums[mid]==nums[mid+1])){
                 high=mid-1;
+            }else{
+                low=mid+1;
             }
-
         }
 
-        return -1;
+
+        return nums[n-1];
         
     }
 };
