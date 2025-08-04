ba7dff8700863e04b082543b6e368e3cf082f205
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-08-04 17:52:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (22.12%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,28 +1,62 @@
 class Solution {
+    vector<int> findlps(string s){
+        int n = s.length();
+        vector<int> lps(n);
+
+        int i=1;
+        int j=0;
+        while(i<n){
+            if(s[i]==s[j]){
+                lps[i]=j+1;
+                i++;
+                j++; 
+            }else
+            if(j>0){
+                j = lps[j-1];
+            }else{
+                lps[i]=0;
+                i++;
+            }
+        }
+
+        return lps;
+    }
+
+    bool kmp(string s, string goal, vector<int>& lps){
+        int n = s.size();
+        int m = goal.size();
+
+        int i=0;
+        int j=0;
+        while(i<n){
+            if(s[i]==goal[j]){
+                i++;
+                j++;
+            }
+            if(j==m) return true;
+            if(i<n && s[i]!=goal[j]){
+                if(j>0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
 public:
     bool rotateString(string s, string goal) {
+
+        // brute - o(nm)
+        // KMP - o(n+m)
+
         int n = s.length();
         int m = goal.length();
 
-        if (n != m) return false;
+        if(n!=m) return false;
 
-        string temp = "";
-        int j = 0;
-
-        while (j < 2 * n) {
-            temp += s[j % n];
-
-            if (temp.length() > m) {
-                for (int k = 1; k < temp.length(); ++k) {
-                    temp[k - 1] = temp[k];
-                }
-                temp.pop_back();
-            }
-
-            if (temp == goal) return true;
-            j++;
-        }
-
-        return false;
+        s+=s;
+        vector<int> lps = findlps(goal);
+        return kmp(s,goal,lps);
     }
 };
