2d0a7d44ac2f3014797accb3a323bb54e6499c86
Anshdeep Shrivastav <114392573+ansh-d23@users.noreply.github.com>
2025-08-08 12:38:21 +0530

    Time: 3 ms (13.23%), Space: 18.7 MB (9.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,47 @@
 class Solution {
-    void dfs(int sr, int sc, vector<vector<int>>& ans, vector<vector<int>>& image, int color, vector<int>& delrow, vector<int>& delcol, int inicolor) {
-        ans[sr][sc] = color;
+    void bfs(int sr, int sc, int color, vector<vector<int>>& image, vector<vector<int>>& vis, vector<vector<int>>& ans){
+        vis[sr][sc]=1;
+
         int n = image.size();
         int m = image[0].size();
 
-        for (int i = 0; i < 4; i++) {
-            int nrow = sr + delrow[i];
-            int ncol = sc + delcol[i];
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == inicolor && ans[nrow][ncol] != color) {
-                dfs(nrow, ncol, ans, image, color, delrow, delcol, inicolor);
+        queue<pair<int,int>> que;
+        que.push({sr,sc});
+
+        while(!que.empty()){
+
+            int size = que.size();
+            vector<int> dx = {1,0,-1,0};
+            vector<int> dy = {0,1,0,-1};
+
+            for(int i=0;i<size;i++){
+                auto [sr,sc] = que.front();
+                ans[sr][sc]=color;
+                que.pop();
+
+                for(int j=0;j<4;j++){
+                    int nr = sr + dx[j];
+                    int nc = sc + dy[j];
+
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]!=1 && image[nr][nc]==image[sr][sc]){
+                        vis[nr][nc]=1;
+                        que.push({nr,nc});
+                    }
+                }
             }
         }
     }
-
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int inicolor = image[sr][sc];
-        if (inicolor == color) return image;
 
+        //bfs
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
         vector<vector<int>> ans = image;
-        vector<int> delrow = {1, 0, -1, 0};
-        vector<int> delcol = {0, 1, 0, -1};
 
-        dfs(sr, sc, ans, image, color, delrow, delcol, inicolor);
+        bfs(sr,sc,color,image, vis, ans);
 
-        return ans;
+        return ans; 
     }
 };
